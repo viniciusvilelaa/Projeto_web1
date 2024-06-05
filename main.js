04b62683105b26256f30c8419e76c562d7533f50
Vinicius Vilela <nino.bez@hotmail.com>
2024-06-05 12:13:38 -0300

    Refinamento nabvar e section contato

--- main.js
+++ main.js
@@ -2,3 +2,11 @@
 const date = new Date();
 const year =date.getFullYear();
 document.getElementById('year').innerHTML = year;
+
+//Nav bar on scroll
+
+window.addEventListener("scroll", function(){
+    const navbar = document.getElementById("navbar");
+    navbar.classList.toggle("sticky", window.scrollY > 0);
+    
+  });
